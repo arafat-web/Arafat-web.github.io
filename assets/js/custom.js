267c65eb6e114c322e6085a3230d169a00997999
Arafat Hossain Ar <arafat.122260@gmail.com>
2021-05-27 18:58:55 +0600

    changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,35 +8,16 @@ $(document).ready(function () {
         $('.loader_bg').fadeToggle();
     }, 2000);
 
-    $(window).on('scroll', function () {
-        if ($(document).scrollTop() > 200) {
-            $(".navbar").css({ "background-color": "#ffffff", "transition": "all 0.5s ease-in-out", "box-shadow": "0px 3px 4.6px 0.3px rgba(0, 0, 0, 0.25)" });
-            $(".navbar-collapse").css("background-color", "transparent");
-        } else {
-            $(".navbar").css({ "background-color": "transparent", "box-shadow": "none" });
-        }
 
-    })
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function () {
+        $(document).on('click', 'a.page-scroll', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
 
-    // function ASB() {
-    //     let k = 1;
-    //     var items = document.querySelectorAll(".skills");
-    //     items.forEach((el) => {
-    //       k = k + 0.2;
-    //       el.style.transition = `all ${k}s ease-out`;
-    //       let limit = el.textContent;
-    //       var width = 0;
-    //       var id = setInterval(frame, 0);
-    //       function frame() {
-    //         if (width >= limit) {
-    //           clearInterval(id);
-    //         } else {
-    //           width++;
-    //           el.style.width = width + "%";
-    //           el.innerHTML = width + "%";
-    //         }
-    //       }
-    //     });
-    //   }
-    //   ASB();
 })
